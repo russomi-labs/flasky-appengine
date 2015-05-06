20b7978a8a7e320a7b86c9b07ca1c0ce40c368ab
russomi <mrusso2@kaplan.edu>
2015-05-06 17:48:19 -0400

    added app.yaml added appengine_config.py for vendoring added SERVER_NAME to testing config to support running tests in PyCharm

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ class TestingConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
     WTF_CSRF_ENABLED = False
-
+    SERVER_NAME = 'localhost:5000'
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
