25a43da7f347ac9d7935d7c05cb89a42133b3e58
russomi <mrusso2@kaplan.edu>
2015-05-07 14:47:11 -0400

    added _send_email_gae() to support sending emails from Google App Engine

--- app/email.py
+++ app/email.py
@@ -1,4 +1,5 @@
 from threading import Thread
+
 from flask import current_app, render_template
 from flask.ext.mail import Message
 from . import mail
@@ -9,7 +10,7 @@ def send_async_email(app, msg):
         mail.send(msg)
 
 
-def send_email(to, subject, template, **kwargs):
+def _send_email(to, subject, template, **kwargs):
     app = current_app._get_current_object()
     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
                   sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
@@ -18,3 +19,25 @@ def send_email(to, subject, template, **kwargs):
     thr = Thread(target=send_async_email, args=[app, msg])
     thr.start()
     return thr
+
+
+def _send_email_gae(to, subject, template, **kwargs):
+    from google.appengine.api import mail
+
+    app = current_app._get_current_object()
+    msg = mail.EmailMessage(sender=app.config['FLASKY_MAIL_SENDER'],
+                            subject=app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject)
+    msg.to = to
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+
+    msg.send()
+
+
+def send_email(to, subject, template, **kwargs):
+    app = current_app._get_current_object()
+
+    if app.config['MAIL_USE_GAE']:
+        return _send_email_gae(to, subject, template, **kwargs)
+    else:
+        return _send_email(to, subject, template, **kwargs)
