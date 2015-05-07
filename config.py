ae4de3f21e3a7b07aa3a8f9b00269837f151c73e
russomi <mrusso2@kaplan.edu>
2015-05-07 14:45:39 -0400

    added MAIL_USE_GAE config to support sending email from Google App Engine

--- config.py
+++ config.py
@@ -7,13 +7,15 @@ class Config:
     SSL_DISABLE = False
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_RECORD_QUERIES = True
+    MAIL_USE_GAE = False
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    # FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    FLASKY_MAIL_SENDER = 'flasky@example.com'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     FLASKY_POSTS_PER_PAGE = 20
     FLASKY_FOLLOWERS_PER_PAGE = 50
@@ -27,19 +29,18 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-    #     'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
     SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/flasky_dev'
+    MAIL_USE_GAE = True
 
 class TestingConfig(Config):
     TESTING = True
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
-    #     'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
     SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/flasky_test'
     WTF_CSRF_ENABLED = False
     SERVER_NAME = 'localhost:5000'
+    MAIL_USE_GAE = True
 
 class ProductionConfig(Config):
+    MAIL_USE_GAE = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 
